725f1731191ea8579eca3be2b4e56e4f0fbf4b9e
chalermsukchanintorn <chalermsukchanintorn@gmail.com>
2025-01-14 14:13:44 +0700

    v.2: create MyClass01, MyClass02 and edit MyClass00

--- src/com/sau/tech/MyClass00.java
+++ src/com/sau/tech/MyClass00.java
@@ -4,5 +4,6 @@ public class MyClass00 {
     //เมธอด main
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Hi, World!");
     }
 }
---
+++ src/com/sau/tech/MyClass01.java
@@ -0,0 +1,7 @@
+package com.sau.tech;
+
+public class MyClass01 {
+    public static void main(String[] args) {
+
+    }
+}
